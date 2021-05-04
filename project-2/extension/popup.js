6a23be162733a7f2099f7dec30cc3f3ab826acd3
mleone26 <78444380+mleone26@users.noreply.github.com>
2021-05-04 11:43:48 -0400

    updates

--- project-2/extension/popup.js
+++ project-2/extension/popup.js
@@ -1,1 +1,18 @@
-// This saves information from the form
+// This JS file is what saves the information from the popup form.
+
+// first define some variables, one for each value you want to save.
+var color;
+
+// The saving happens when the user clicks "save"
+document.getElementById("save").onclick = function() {
+
+  // step 2: get the selected color, by finding that element's value
+  color = document.getElementById("color").value;
+
+  // step 3: save it to local storage
+  
+   chrome.storage.local.set({"color":color}, function(){
+    console.log("saved to storage favorite color " + color);
+  });
+}
+
--- project-2/extension/update.js
+++ project-2/extension/update.js
@@ -1,1 +1,17 @@
-// This runs the extension and gets information from the form
+// This javascript file is what changes the display of the webpage.
+// It works by retrieving the values stored in local storage,
+// and then using those values to change the CSS.
+
+var all = document.querySelectorAll("*");
+
+// All that updating is done in one function.
+// this function fires when the local storage has been updated or changed.
+chrome.storage.local.onChanged.addListener(function() {
+  console.log("changed");
+  chrome.storage.local.get(["color"], function(data) {
+  	var html = document.querySelectorAll("html")[0];
+  	console.log(html);
+  	console.log(data["color"]);
+  	html.classList.add(data["color"]);
+  });
+});
